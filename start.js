f64e810f3e2fa43c95ba6b05a2d11002de9867a0
hasan <hasanredzo@gmail.com>
2020-01-06 14:52:42 +0100

    test

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ var http = require('http');
 var bhtml = require('./modules/basic');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/html'});
     bhtml.blkHTML(res);
     res.write('Hello World!');
     res.end();
